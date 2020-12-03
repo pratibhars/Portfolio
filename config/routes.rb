5beda6c0a40c0bd5b67459442ce7a50397b0ce17
Pratibha <68182689+pratibhars@users.noreply.github.com>
2020-12-03 14:37:25 +1000

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'portfolio/home'
+  get 'portfolio/home', to: 'portfolio#index'
+  root to: 'portfolio#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
