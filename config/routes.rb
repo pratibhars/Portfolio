09e2e043b5965b971bb75c54e4a923dc8f9e63db
Pratibha <68182689+pratibhars@users.noreply.github.com>
2020-12-03 16:05:14 +1000

    added bootstrap template

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'portfolio/home', to: 'portfolio#index'
-  root to: 'portfolio#index'
+  root to: 'portfolio#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
